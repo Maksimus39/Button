c3065ad200455f3f710c2143607795855376d6b3
Maksim <maksim.minakov.83@mail.ru>
2023-05-11 21:22:25 +0300

    1.Button

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+   /* const myFirstSubscriber =(event:React.MouseEvent<HTMLButtonElement>)=>{
+        console.log('hello VASYA!')
+    }
+    const mySecondSubscriber=(event:React.MouseEvent<HTMLButtonElement>)=>{
+        console.log('hello ivan')
+    }*/
+    const  onClickHandler=(name: string)=>{
+        console.log(name)
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <button onClick={(event)=>{console.log('hello')}}>MyYouTubeChannel-1</button>*/}
+     <button onClick={(event:React.MouseEvent<HTMLButtonElement>)=>onClickHandler('VASYA')}>MyYouTubeChannel-1</button>
+     <button onClick={(event:React.MouseEvent<HTMLButtonElement>)=>onClickHandler('ivan')}>MyYouTubeChannel-2</button>
     </div>
   );
 }
